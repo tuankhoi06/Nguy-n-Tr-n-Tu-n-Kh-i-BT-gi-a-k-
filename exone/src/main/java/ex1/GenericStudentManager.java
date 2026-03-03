1072eb44849e6624241f5acc6163243a2797d0bb
tuankhoi06 <tuankhoi06.hg@gmail.com>
2026-03-03 08:55:54 +0700

    Add files via upload

---
+++ exone/src/main/java/ex1/GenericStudentManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex1;
+
+/**
+ *
+ * @author Admin
+ */
+public class GenericStudentManager {
+        ArrayList<student> danhSach = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+}
---
+++ exone/src/main/java/ex1/Student.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ex1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        private string id;
+        private string name;
+        private doulbe gpa;
+        
+    public student (string id, string name, doulbe id);
+        this.id = id;
+        this.name=name;
+        this.gpa=gpa;
+        
+        public String getid() { return id; }
+        public String getname() { return name; }
+        public Double getgpa() { return gpa; }
+
+
+        @Overdrive
+        public string tostring (){
+            return "id: " + id + " | name: " + name + " | gpa: " + gpa;
+        }
+        
+    }
